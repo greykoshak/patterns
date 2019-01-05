24ae5bfa456e3d71060968d9f79f7f89c672ca93
greykoshak <greykoshak@gmail.com>
2019-01-05 23:47:54 +0200

    added factorize test done!

--- factorize.py
+++ factorize.py
@@ -9,46 +9,46 @@ def factorize(x):
 
 class TestFactorize(unittest.TestCase):
     def test_wrong_types_raise_exception(self):
-        with self.subTest(x=1):
-            self.assertRaises(TypeError, factorize, "string")
-        with self.subTest(x=2):
-            self.assertRaises(TypeError, factorize, 1.5)
+        self.cases = [1.5, "string"]
+        for val in self.cases:
+            with self.subTest(x=val):
+                self.assertRaises(TypeError, factorize, val)
 
     def test_negative(self):
-        with self.subTest(x=1):
-            self.assertRaises(ValueError, factorize, -1)
-        with self.subTest(x=2):
-            self.assertRaises(ValueError, factorize, -10)
-        with self.subTest(x=3):
-            self.assertRaises(ValueError, factorize, -100)
+        self.cases = [-1, -10, -100]
+        for val in self.cases:
+            with self.subTest(x=val):
+                self.assertRaises(ValueError, factorize, val)
 
     def test_zero_and_one_cases(self):
-        with self.subTest(x=1):
-            self.assertTupleEqual(factorize(0), tuple([0]))
-        with self.subTest(x=2):
-            self.assertTupleEqual(factorize(1), tuple([1]))
+        self.cases = [0, 1]
+        self.wait_results = [(0,), (1,)]
+        for val, tuple_x in zip(self.cases, self.wait_results):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), tuple_x)
 
     def test_simple_numbers(self):
-        with self.subTest(x=1):
-            self.assertTupleEqual(factorize(3), tuple([3]))
-        with self.subTest(x=2):
-            self.assertTupleEqual(factorize(13), tuple([13]))
-        with self.subTest(x=3):
-            self.assertTupleEqual(factorize(29), tuple([29]))
+        self.cases = [3, 13, 29]
+        self.wait_results = [(3,), (13,), (29,)]
+        for val, tuple_x in zip(self.cases, self.wait_results):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), tuple_x)
+
 
     def test_two_simple_multipliers(self):
-        with self.subTest(x=1):
-            self.assertTupleEqual(factorize(6), tuple([2, 3]))
-        with self.subTest(x=2):
-            self.assertTupleEqual(factorize(26), tuple([2, 13]))
-        with self.subTest(x=3):
-            self.assertTupleEqual(factorize(121), tuple([11, 11]))
+        self.cases = [6, 26, 121]
+        self.wait_results = [(2, 3), (2, 13), (11, 11)]
+        for val, tuple_x in zip(self.cases, self.wait_results):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), tuple_x)
 
     def test_many_multipliers(self):
-        with self.subTest(x=1):
-            self.assertTupleEqual(factorize(1001), tuple([7, 11, 13]))
-        with self.subTest(x=2):
-            self.assertTupleEqual(factorize(9699690), tuple([2, 3, 5, 7, 11, 13, 17, 19]))
+        self.cases = [1001, 9699690]
+        self.wait_results = [(7, 11, 13), (2, 3, 5, 7, 11, 13, 17, 19)]
+        for val, tuple_x in zip(self.cases, self.wait_results):
+            with self.subTest(x=val):
+                self.assertTupleEqual(factorize(val), tuple_x)
+
 
 if __name__ == "__main__":
     unittest.main()
